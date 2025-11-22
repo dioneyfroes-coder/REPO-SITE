50468d15e5ed8ae33810957af284c05870abaf96
dioneyfroes <dioney.froes@hotmail.com>
2025-11-21 21:06:01 -0300

    fix: home

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/backbet" />;
+  return <Redirect to="/docs/backbet/overview" />;
 }
